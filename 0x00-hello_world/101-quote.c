099a73894e696680eebf9b89d6e00f05eb466196
KoeusIss <sebri.issam@gmail.com>
2020-02-05 14:52:03 +0000

    commit the answer of the second advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - return a ginius simplicity
  * Return: 1
  */
 int main(void)
 {
-	char *c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char string[] = \
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	while (*c != '\0')
-	{
-		putchar(*c);
-		c++;
-	}
+	write(1, string, sizeof(string));
 	return (1);
 }
