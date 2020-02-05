5316a72ea6490bcf93b2a361c3f34d9a72449408
KoeusIss <sebri.issam@gmail.com>
2020-02-05 23:41:20 +0100

    Fic quote task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 /**
- * main - return a ginius simplicity
+ * main - main function
+ *
  * Return: 1
  */
 int main(void)
 {
-	char string[] =
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *string;
+	char str1[] = "and that piece of art is useful\"";
+	char str2[] = "- Dora Korpar, 2015-10-2019";
 
+	string = malloc(strlen(str1) + strlen(str2) + 1);
+	strcpy(string, str1);
+	strcpy(string, str2);
 	write(2, string, sizeof(string));
-	return (1);
 }
