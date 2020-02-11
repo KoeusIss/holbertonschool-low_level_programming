d3af83353004dac054026e6ac31839a967715b6a
KoeusIss <sebri.issam@gmail.com>
2020-02-11 10:27:29 +0100

    Commit the task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - print out the "Holberton" word
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c[] = "Holberton";
+
+	while (i < 10)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
