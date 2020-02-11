41d84d2fb0b3baf9025da60c45485389eac4567b
KoeusIss <sebri.issam@gmail.com>
2020-02-11 13:06:50 +0100

    Commit the task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - add two number
+ * @arg1: the operand
+ * @arg2: second operand
+ *
+ * Return: result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
