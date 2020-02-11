5fe3f8ee33121f3ef7df3a376afc8819c2e8aef9
KoeusIss <sebri.issam@gmail.com>
2020-02-11 11:05:27 +0100

    Commit the answer of the task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - print out the absolute value of number
+ * @n: the number
+ *
+ * Return: int absolute value
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return ((-1) * n);
+	}
+	return (n);
+}
