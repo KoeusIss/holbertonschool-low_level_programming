984fb6bea72c2f19b7d2f60032c63ee8fb024a3d
KoeusIss <sebri.issam@gmail.com>
2020-02-12 14:08:57 +0100

    Commit the 103 task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print out the first 50 fibonacci
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long fib0 = 1;
+	long fib1 = 2;
+	long fib;
+	long sum = fib0 + fib1;
+
+	for (i = 3; i <= 4000000; i++)
+	{
+		fib = fib0 + fib1;
+		sum += fib;
+
+		fib0 = fib1;
+		fib1 = fib;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
