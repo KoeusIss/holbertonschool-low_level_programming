571fe3f38f1f0e9cbb738766ccc467242d3c9680
KoeusIss <sebri.issam@gmail.com>
2020-02-18 08:37:16 +0100

    Commit the second second task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Update the value of parameter to 98
+ * @num: parameter to be reset
+ *
+ * Return: void
+ */
+void reset_to_98(int *num)
+{
+	*num = 98;
+}
