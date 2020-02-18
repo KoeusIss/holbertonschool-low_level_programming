aab2326f1a5d09e3899b3b54714e0d60458d4412
KoeusIss <sebri.issam@gmail.com>
2020-02-18 11:48:52 +0100

    Commit the 9th task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print an amount of array's element
+ * @n: amount of elements to be printed
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n-1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
