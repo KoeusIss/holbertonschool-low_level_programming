fabf37936995c889b65c89eb9e3c07fccc22b9f8
KoeusIss <sebri.issam@gmail.com>
2020-02-18 13:37:08 +0100

    Fix the betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,8 +12,8 @@ char *_strcpy(char *dest, char *source)
 
 	for (i = 0; source[i] != '\0'; i++)
 	{
-		dest[i] = source [i];
+		dest[i] = source[i];
 	}
 	dest[i] = '\0';
-	return dest;
+	return (dest);
 }
