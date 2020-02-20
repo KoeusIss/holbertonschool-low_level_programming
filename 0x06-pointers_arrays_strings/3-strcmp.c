b5d25ffe63c693a24503b31d9a661ece943ef0d4
KoeusIss <sebri.issam@gmail.com>
2020-02-20 11:50:28 +0100

    Commit the answer of the 4th task  - string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: The differnce between two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int cmp = 0;
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			cmp = s1[i] - s2[i];
+			break;
+		}
+		else
+			continue;
+	}
+	return cmp;
+}
