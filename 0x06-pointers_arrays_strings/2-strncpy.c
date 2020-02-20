9b0780b201ca8d35ca935dcf98760b3d6ec6765e
KoeusIss <sebri.issam@gmail.com>
2020-02-20 13:44:57 +0100

    Commit the 7th task - Capitalize string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && dest[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
+	while (dest[i] != '\0')
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * cap_string - Capitalize a string
+ * @str: the ginven string
+ *
+ * Return: A Capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	char seperator[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '(', ')', '{', '}', '"'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if ((i == 0 || str[i - 1] == seperator[j]) && (str[i] >= 'a' && str[i] <= 'z'))
+				str[i] -= 32;
+		}
+	}
+	return (str);
+}
