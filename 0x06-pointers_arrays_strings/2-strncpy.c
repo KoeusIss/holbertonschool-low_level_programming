f3cfd060b1b928715de3dcfe76f3c59719d48b1b
KoeusIss <sebri.issam@gmail.com>
2020-02-20 20:20:26 +0100

    Commit the answer of the 3rd task - strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (dest[i] != '\0')
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
