342f9c2979b5fbeb8c7af866d90556df8cd4c53e
KoeusIss <sebri.issam@gmail.com>
2020-02-20 13:50:25 +0100

    Fix the over 80 character in betty style

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,15 @@ char *cap_string(char *str)
 {
 	int i;
 	int j;
-	char seperator[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '(', ')', '{', '}', '"'};
+	char seperator[] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'(', ')', '{', '}', '"'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 13; j++)
 		{
-			if ((i == 0 || str[i - 1] == seperator[j]) && (str[i] >= 'a' && str[i] <= 'z'))
+			if ((i == 0 || str[i - 1] == seperator[j]) &&
+					(str[i] >= 'a' && str[i] <= 'z'))
 				str[i] -= 32;
 		}
 	}
