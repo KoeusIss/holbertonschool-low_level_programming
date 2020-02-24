64ae6723e38882441a87b36004a47816f9e25b63
KoeusIss <sebri.issam@gmail.com>
2020-02-24 10:16:28 +0100

    Commit the answer of the third task - strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strchr - Locates a character in a string
+ * @str: Given string
+ * @c: Given character
+ *
+ * Return: Return a pointer to the fist occurence of c
+ */
+char *_strchr(char *str, char c)
+{
+	while (*str != '\0')
+	{
+		if (*str == c)
+			break;
+		str++;
+	}
+	return (str);
+}
