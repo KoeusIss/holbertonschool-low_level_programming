dbc4981c2bed45354be920bab7037dbcaf227b23
KoeusIss <sebri.issam@gmail.com>
2020-02-29 13:39:21 +0100

    Commit the answer of the fourth task - multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+/**
+ * print_int - print integer
+ * @num: the given integer
+ *
+ * Return: void
+ */
+void print_int(int num)
+{
+	if (num / 10)
+		print_int(num / 10);
+	_putchar(num % 10 + '0');
+}
+/**
+ * _atoi - converts character to integer
+ * @c: the given character
+ *
+ * Return: An integer
+ */
+int _atoi(char *c)
+{
+	int val = 0;
+
+	while (*c)
+	{
+		val = (val * 10) + (*c - '0');
+		c++;
+	}
+	return (val);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: Arguments count
+ * @argv: Arguments vector
+ *
+ * Return: 0 if exit with success, 1 exit with error
+ */
+int main(int argc, char **argv)
+{
+	int mul;
+	char *e;
+
+	e = "Error";
+	if ((argc - 1) == 2)
+	{
+		mul = _atoi(argv[1]) * _atoi(argv[2]);
+		print_int(mul);
+		_putchar('\n');
+		return (0);
+	}
+	else
+	{
+		while (*e)
+			_putchar(*e++);
+		_putchar('\n');
+		return (1);
+	}
+}
