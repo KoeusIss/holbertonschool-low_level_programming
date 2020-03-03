09b39755a5fd09fab8a17b2c1541fb54d3301977
KoeusIss <sebri.issam@gmail.com>
2020-03-03 13:58:19 +0100

    Commit the alloc_grid task and the header file

---
+++ 0x0B-malloc_free/3-alloc-grid.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates a two dimensional grid with 0 filling
+ * @width: the width of the grid
+ * @height: the height of the grid
+ *
+ * Return: a pointer to the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **grid;
+
+	/* Checks the null sizes */
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	/* Allocates a memory for grid */
+	grid = (int **) malloc(width * sizeof(int));
+	if (grid == NULL)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		grid[i] = (int *) malloc(height * sizeof(int));
+		i++;
+	}
+	/* fill the grid */
+	i = 0;
+	j = 0;
+	while (i < height)
+	{
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (grid);
+}
