b9c77d7fd68bcaa3450ba448c28914dda97efce3
KoeusIss <sebri.issam@gmail.com>
2020-03-05 17:50:56 +0100

    Fix the betty style

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * fill_an_array - fill an array with elements
+ * @a: the given array
+ * @el: the given element
+ * @len: the length of the array
+ *
+ * Return: pointer to filled array
+ */
 void *fill_an_array(void *a, int el, unsigned int len)
 {
 	char *p = a;
