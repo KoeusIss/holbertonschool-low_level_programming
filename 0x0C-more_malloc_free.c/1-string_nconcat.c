1452e9d438579d2fa187146f0f11f52e96ce83d3
KoeusIss <sebri.issam@gmail.com>
2020-03-05 12:27:20 +0100

    Commit the string_nconcat file

---
+++ 0x0C-more_malloc_free.c/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - determine the string length
+ * @str: the given string
+ *
+ * Return: integer length
+ */
+int _strlen(char *str)
+{
+	/* Declare the return length */
+	int len = 0;
+
+	/* Checks the null argument */
+	if (str == NULL)
+		return (len);
+	/* Determine the length of string */
+	while (*(str + len))
+		len++;
+	/* Final return */
+	return (len);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first given string
+ * @s2: second given string
+ * @n: amount of bytes from the second string
+ *
+ * Return: a pointer to the newly concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	/* Declaration */
+	char *result;
+	unsigned int l1 = _strlen(s1);
+	unsigned int l2 = _strlen(s2);
+	unsigned int i;
+
+	/* Checks fi n exceed the string length */
+	if (n >= l2)
+		n = l2;
+	/* Allocate memory for the result */
+	result = malloc(sizeof *result * (l1 + n + 2));
+	if (result == NULL)
+		return (NULL);
+	/* Filling the result array */
+	for (i = 0; i < l1; i++)
+		result[i] = s1[i];
+	for (i = 0; i < n; i++)
+		result[l1 + i] = s2[i];
+	/* Final return */
+	return (result);
+}
