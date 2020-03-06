0e9f2003f8c807e4387f4ba181341c41d6af8a48
KoeusIss <sebri.issam@gmail.com>
2020-03-06 23:14:48 +0100

    Commit the mul task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - determines the string length
+ * @s: given string
+ *
+ * Return: length integer
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while(*s != '\0')
+		len++;
+	return (len);
+}
 
 /**
  * _error - prints error
@@ -30,28 +44,33 @@ int _is_digit(char c)
 }
 
 /**
- * _atoi - converts array to integer
- * @str: given array
+ * _chartoi - converts charcter to integer
+ * @c: given character
  *
  * Return: integer
  */
-int _atoi(char *str)
+int _chartoi(char c)
 {
-	int val = 0;
+	if (_is_digit(c))
+		return (c - '0');
+	else
+		return (0);
+}
 
-	while (*str)
-	{
-		if (_is_digit(*str))
-		{
-			val = val * 10 + (*str - '0');
-		}
-		else
-		{
-			return (0);
-		}
-		str++;
-	}
-	return (val);
+/**
+ * print_arr - prints array of string
+ * @arr: given string
+ *
+ * Return: void
+ */
+void print_arr(char *arr)
+{
+	int i;
+	int len;
+
+	len = _strlen(arr);
+	for (i = 0; i < len; i++)
+		_putchar(arr[i] + '0');
 }
 
 /**
@@ -61,26 +80,52 @@ int _atoi(char *str)
  *
  * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(int ac __attribute__((unused)), char **av)
 {
-	int num1;
-	int num2;
-	unsigned long int result;
+	char *result;
+	int l1;
+	int l2;
+	int carry;
+	int i;
+	int j;
+	int tmp;
+	int a, b;
 
-	if (argc != 3)
+	/* Checks the length of arguments */
+	if (ac != 3)
 	{
 		_error();
 		exit(98);
 	}
-	
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	if (!num1 || !num2)
+	/* Determine the length of the two arguments */
+	l1 = _strlen(av[1]);
+	l2 = _strlen(av[2]);
+	/* Allocate space for result */
+	result = malloc(l1 + l2);
+	if (result == NULL)
 	{
 		_error();
 		exit(98);
 	}
-	result = num1 * num2;
-	printf("%lu", result);
+	/* Fill the result */
+	for (i = l1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = l2 - 1; j >= 0; j++)
+		{
+			a = _chartoi(av[1][i]);
+			b = _chartoi(av[2][j]);
+			if (!a || !b)
+			{
+				_error();
+				exit(98);
+			}
+			tmp += carry + a * b + result[i + j + 1];
+			carry = result[i + j - 1] / 10;
+			result[i + j - 1] = (tmp % 10) + '0';
+		}
+		result[i + j + 1] += carry;
+	}
+	print_arr(result);
 	return (0);
 }
