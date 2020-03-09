e057a6ed5569d35313471af91bd1076ef427cfeb
KoeusIss <sebri.issam@gmail.com>
2020-03-09 18:06:29 +0100

    Commit the fourth task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,8 +12,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	char *my_name = name;
-	char *my_owner = owner;
+	char *my_name;
+	char *my_owner;
+	int l1 = 0;
+	int l2 = 0;
+	int i;
+
+	while (name[l1])
+		l1++;
+	while (owner[l2])
+		l2++;
+
+	my_name = malloc(l1 + 1);
+	if (my_name == NULL)
+		return (NULL);
+	i = 0;
+	while (i < l1)
+	{
+		my_name[i] = name[i];
+		i++;
+	}
+	my_name[i] = '\0';
+
+	my_owner = malloc(l2 + 1);
+	if (my_owner == NULL)
+		return (NULL);
+	i = 0;
+	while (i < l2)
+	{
+		my_owner[i] = owner[i];
+		i++;
+	}
+	my_owner[i] = '\0';
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
