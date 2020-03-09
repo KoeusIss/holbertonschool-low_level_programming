353e67bee644e72d5a000fedb5297a1fee95b471
KoeusIss <sebri.issam@gmail.com>
2020-03-09 14:33:04 +0100

    Commit the header file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Django is a nice dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: this is a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
