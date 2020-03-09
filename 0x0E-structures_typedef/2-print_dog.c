419190935e06349793343e990a2c7d8528442581
KoeusIss <sebri.issam@gmail.com>
2020-03-09 16:46:44 +0100

    Fix the null given struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,20 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d->name)
-		printf("Name: %s\n", d->name);
-	else
-		printf("Name: (nil)");
-	if (d->age)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)");
-	if (d->owner)
-		printf("Owner: %s\n", d->owner);
-	else
-		printf("Owner: (nil)");
+	if (d != NULL)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)");
+		if (d->age)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)");
+	}
+	return (NULL);
 }
