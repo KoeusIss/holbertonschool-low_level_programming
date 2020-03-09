870e7398628d73ad582f76ba03ce97677d2ad034
KoeusIss <sebri.issam@gmail.com>
2020-03-09 14:46:36 +0100

    Commit the inti_dog function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialise a dog structure
+ * @d: a dog struture
+ * @name: a dog's name
+ * @age: a dog's age
+ * @owner: a dog's owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
