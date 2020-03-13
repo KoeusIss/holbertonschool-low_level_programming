c8260ce1e9f8d07b463f44a556da747070c6a847
KoeusIss <sebri.issam@gmail.com>
2020-03-13 17:31:42 +0100

    Fix the nil return of the string

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -35,7 +35,15 @@ void print_float(va_list ap)
  */
 void print_string(va_list ap)
 {
-	printf("%s", va_arg(ap, char *));
+	char *s;
+
+	s = va_arg(ap, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
 }
 
 /**
