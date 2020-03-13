1fa700b96c57f94ae08b51bc3e21e70c5710791c
KoeusIss <sebri.issam@gmail.com>
2020-03-13 12:24:06 +0100

    Commit the third task print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @seperator - seperator between strings
+ * @n: the sount of strings
+ *
+ * Return: void
+ */
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < n - 1 && seperator)
+			printf("%s", seperator);
+	}
+	va_end(ap);
+	printf("\n");
+}
