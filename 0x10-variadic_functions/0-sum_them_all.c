e1b064890200368273e19f33814f40b7084495ca
KoeusIss <sebri.issam@gmail.com>
2020-03-13 11:15:56 +0100

    Commit the first task sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all the paremeters
+ * @n: the given numbers
+ *
+ * Return: the sul integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
