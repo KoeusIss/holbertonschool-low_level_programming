8ac1a78dead4d80a222df87e418c8478be15d2cf
KoeusIss <sebri.issam@gmail.com>
2020-03-19 11:46:17 +0100

    Commit the fourth task add at the end of the list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * _strlen - determines the string length
+ * @str: given string
+ *
+ * Return: an integer length
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[++len])
+		;
+	return (len);
+}
+
+/**
+ * add_node_end - add a node at the end of list
+ * @head: the head of the list
+ * @str: a given string
+ *
+ * Return: a pointer to the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *tmp;
+
+	tmp = *head;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = new_node;
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,6 +30,7 @@ int _putchar(char);
 size_t print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
 
 
 #endif /* LISTS_H */
