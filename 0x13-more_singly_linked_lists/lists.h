adfb1f5eba97937d781d2c714763bc03e65c967a
KoeusIss <sebri.issam@gmail.com>
2020-03-23 15:11:20 +0100

    Commit the head file list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+/* Functions */
+int _putchar(char);
+size_t print_listint(const listint_t *);
+size_t listint_len(const listint_t *);
+
+#endif
