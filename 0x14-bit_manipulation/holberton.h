71bb7582ec6be84e3d1fa5ea674732cdbdb1f82c
KoeusIss <sebri.issam@gmail.com>
2020-03-26 16:26:37 +0100

    Commit the task 5 flip counter

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -12,5 +12,6 @@ void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif /* HOLBERTON_H */
