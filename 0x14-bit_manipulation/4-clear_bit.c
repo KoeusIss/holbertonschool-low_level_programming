c915c79f822d267a301c118255da1add837f1e59
KoeusIss <sebri.issam@gmail.com>
2020-03-26 14:57:30 +0100

    fix the edge case for over range index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,8 +9,14 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	if (index > sizeof(n) * 8)
+	{
 		return (-1);
+	}
+	if (n == NULL)
+	{
+		return (-1);
+	}
 	*n &= ~(1 << index);
 	return (1);
 }
