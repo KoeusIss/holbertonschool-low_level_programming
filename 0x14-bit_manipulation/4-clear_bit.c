eafc83f41c9c778f465de9ce595de485172f4007
KoeusIss <sebri.issam@gmail.com>
2020-03-26 14:53:45 +0100

    Commit the answer of the fifth task - clear a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: the given number
+ * @index: the given index
+ *
+ * Return: (1) if succeed, (-1) otherwise
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+	
+	*n &= ~(1 << index);
+	return (1);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -11,5 +11,6 @@ unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* HOLBERTON_H */
