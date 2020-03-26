08fdb331b725e42d2f55c7cd234690485207c06f
KoeusIss <sebri.issam@gmail.com>
2020-03-26 11:25:15 +0100

    Commit the header file holberton

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes */
+int _putchar(char);
+unsigned int binary_to_uint(const char *);
+
+#endif /* HOLBERTON_H */
