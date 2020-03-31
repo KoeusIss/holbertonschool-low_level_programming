9acc4dd3f34aa847ee23b7b80ea7f86586190ca9
KoeusIss <sebri.issam@gmail.com>
2020-03-31 15:43:50 +0100

    Fix the text content condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,15 +17,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd < 0)
 		return (-1);
-	while (text_content[len])
-		len++;
 
-	if (len > 0)
+	if (text_content)
 	{
+		while (text_content[len])
+			len++;
 		w = write(fd, text_content, len);
 		if (w < 0)
 			return (-1);
 	}
-
 	return (1);
 }
