ceab3369c111139d0d40e5de749dfaff2b24ec6a
KoeusIss <sebri.issam@gmail.com>
2020-03-31 19:34:46 +0100

    Fix the task three

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,30 +10,29 @@
 int cp(char *file_to, char *file_from)
 {
 	char *buffer[1024];
-	ssize_t file_to_d, file_from_d, file_from_w, file_to_w;
+	int file_to_d, file_from_d, file_from_r, file_to_w;
 	int file_from_c, file_to_c;
 
 	file_from_d = open(file_from, O_RDONLY);
 	if (file_from_d < 0)
 		return (98);
 
-	file_to_d = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
+	file_to_d = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to_d < 0)
 		return (99);
 
-	file_from_w = read(file_from_d, buffer, 1024);
-	if (file_from_w < 0)
+	file_from_r = read(file_from_d, buffer, 1024);
+	if (file_from_r < 0)
 		return (98);
 
-	while (file_from_w > 0)
+	while (file_from_r > 0)
 	{
-		file_to_w = write(file_to_d, buffer, file_from_w);
+		file_to_w = write(file_to_d, buffer, file_from_r);
 		if (file_to_w < 0)
 			return (99);
-		file_from_w = read(file_from_d, buffer, 1024);
-		if (file_from_w < 0)
+		file_from_r = read(file_from_d, buffer, 1024);
+		if (file_from_r < 0)
 			return (98);
-		
 	}
 
 	file_from_c = close(file_from_d);
@@ -50,6 +49,7 @@ int cp(char *file_to, char *file_from)
 	}
 	return (0);
 }
+
 /**
  * main - the main function
  * @ac: the argument count
@@ -62,10 +62,7 @@ int main(int ac, char **av)
 	int code;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	code = cp(av[2], av[1]);
 	switch (code)
