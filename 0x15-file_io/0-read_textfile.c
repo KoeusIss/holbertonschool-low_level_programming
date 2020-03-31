c58aa128f1841b5ccd6756b0b9f2f9ad2d2f7565
KoeusIss <sebri.issam@gmail.com>
2020-03-31 14:31:10 +0100

    Fix the betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,27 +10,23 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n;
+	ssize_t r, w;
 	char *buf;
 
 	buf = malloc((letters + 1) * sizeof(char));
 	if (!buf)
 		return (0);
-	
 	if (!filename)
 		return (0);
-	
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-
-	n = read(fd, buf, letters);
+	r = read(fd, buf, letters);
 	buf[letters] = '\0';
-
 	close(fd);
-
-	write(1, buf, letters);
-	
+	w = write(1, buf, letters);
+	if (w != r || w < 0)
+		return (0);
 	free(buf);
-	return (n);
+	return (r);
 } /* read_textfile */
