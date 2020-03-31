3dc36f8f84a77c8c91055b4fd35ca7f277080b3f
KoeusIss <sebri.issam@gmail.com>
2020-03-31 15:58:36 +0100

    Commit the third task

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,5 +13,6 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
 
 #endif /* HOLBERTON_H */
