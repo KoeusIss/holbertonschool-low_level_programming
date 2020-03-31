d5011a750a126c79d691982005b21e9fad3e1f49
KoeusIss <sebri.issam@gmail.com>
2020-03-31 12:45:18 +0100

    Commit the header file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Header file */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* Prototypes */
+int _putchar(char c);
+ssize_t read_textfile(const char *, size_t);
+
+#endif /* HOLBERTON_H */
