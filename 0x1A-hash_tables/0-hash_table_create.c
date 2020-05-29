d353c91df49aae9acde3b1e63548e2bda80e6d2e
KoeusIss <sebri.issam@gmail.com>
2020-05-29 12:33:01 +0100

    Fix the first task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,15 +12,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	ht = malloc(sizeof(hash_table_t));
+	ht = malloc(1 * sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = malloc(sizeof(hash_node_t *));
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
-	ht->size = size;
 	return (ht);
 }
