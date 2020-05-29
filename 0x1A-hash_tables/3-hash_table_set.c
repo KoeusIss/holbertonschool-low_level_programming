911d0d580b89341623caef46e93b3091a63fca8f
KoeusIss <sebri.issam@gmail.com>
2020-05-29 16:29:32 +0100

    Fix the non collision override

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,15 +12,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *tmp;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (!strcmp(key, tmp->key))
+		{
+			tmp->value = strdup(value);
+			if (!tmp->value)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
