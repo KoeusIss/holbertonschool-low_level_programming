0e6dc84bd51bb130393795957190f788fe52c8c3
KoeusIss <sebri.issam@gmail.com>
2020-05-29 13:55:38 +0100

    fi the 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	new_node->value = strdup(value);
+	if (value)
+		new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node);
