d5dbd1e323347c0254f25b671bf8f808652323ee
KoeusIss <sebri.issam@gmail.com>
2020-05-29 14:12:26 +0100

    fix the 3thrd

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,12 +11,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	ul index;
+	unsigned long int index;
 	hash_node_t *new_node;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
-	index = key_index((cuc *) key, ht->size);
+	index = key_index((const unsigned char *) key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
