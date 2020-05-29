68f1fb07f2031cb5a4d91e9dbda762c97d64cd70
KoeusIss <sebri.issam@gmail.com>
2020-05-29 14:10:56 +0100

    fix the cast

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,12 +11,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node = NULL;
+	ul index;
+	hash_node_t *new_node;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
-	index = key_index((void *) key, ht->size);
+	index = key_index((cuc *) key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
