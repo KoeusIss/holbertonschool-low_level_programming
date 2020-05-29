3e4ad7b59bd71d7fa3566cffd7bfe682cc25bf96
KoeusIss <sebri.issam@gmail.com>
2020-05-29 13:17:09 +0100

    check the hash table for free

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,12 +33,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node->key);
 		return (0);
 	}
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-		new_node->next = NULL;
-		return (1);
-	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
