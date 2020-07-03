9a22b804315fec0eb2e75d9d68556b4969be3d16
KoeusIss <sebri.issam@gmail.com>
2020-07-03 16:21:20 +0100

    commit the interview

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,14 +8,19 @@ def island_perimeter(grid):
     """
         Returns the perimeter of given island
     """
-    l = len(grid)
+    w = len(grid[0])
+    h = len(grid)
     per = 0
     for i, col in enumerate(grid):
         for j, row in enumerate(col):
             if row == 1:
                 per += 4
-                if j > 0:
-                    if grid[i][j-1] = 1: per -= 1
-                if grid[i][j+1] = 1: per -= 1
-                if grid[i+1][j] = 1: per -= 1
-                if grid[i-1][j] = 1: per -= 1
+                if grid[i][j-1] == 1:
+                    per -= 1
+                if grid[i][(j+1) % w] == 1:
+                    per -= 1
+                if grid[(i+1) % h][j] == 1:
+                    per -= 1
+                if grid[i-1][j] == 1:
+                    per -= 1
+    return per
